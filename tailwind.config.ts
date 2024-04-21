3e313d9635cef907f4545849488ff7882355eeca
Pavel Volek <me@pavelvolek.cz>
2024-04-21 17:31:09 +0200

    Add splash screen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        gradient:
+          "radial-gradient(circle at center bottom, rgba(54,0,188,1) 0%, rgba(54,0,188,0) 60%),radial-gradient(circle at left 35%, rgba(100,0,247,1) 0%, rgba(100,0,247,0) 35%)",
       },
     },
   },
