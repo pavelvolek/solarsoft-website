2d635bc678467031136bcf9032d4c562ccfe7ce8
Pavel Volek <me@pavelvolek.cz>
2024-11-24 12:36:48 +0100

    Fix deps

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import NaseReseni from './ui/nase-reseni';
-import Contact from './ui/contact-v2';
-import EnergyManagement from './ui/energy-management';
-import Footer from './ui/footer';
-import Header from './ui/header';
-import HeaderStrip from './ui/header-strip';
-import HeroSection from './ui/hero-section';
-import DispecerskeRizeni from './ui/dispecerske-rizeni';
-import SectionRTU from './ui/section-rtu';
+// import NaseReseni from './ui/nase-reseni';
+// import Contact from './ui/contact-v2';
+// import EnergyManagement from './ui/energy-management';
+// import Footer from './ui/footer';
+// import Header from './ui/header';
+// import HeaderStrip from './ui/header-strip';
+// import HeroSection from './ui/hero-section';
+// import DispecerskeRizeni from './ui/dispecerske-rizeni';
+// import SectionRTU from './ui/section-rtu';
 import Logo from './ui/solarsoft-logo';
 
 export default function Home() {
